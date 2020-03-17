6bccf8817d8f4c2c7a55b15fb218acdc7e08cad4
hjzheng <hjzheng@yuansuan.cn>
2020-03-17 22:04:08 +0800

    2020-03-17

---
+++ 384.打乱数组.js
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=384 lang=javascript
+ *
+ * [384] 打乱数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ */
+var Solution = function(nums) {
+  this.nums = nums;
+};
+
+/**
+ * Resets the array to its original configuration and return it.
+ * @return {number[]}
+ */
+Solution.prototype.reset = function() {
+  return this.nums;
+};
+
+/**
+ * Returns a random shuffling of the array.
+ * @return {number[]}
+ */
+Solution.prototype.shuffle = function() {
+  // Fisher-Yates 洗牌算法
+  let arr = [...this.nums];
+
+  let i = arr.length;
+  let j = 0;
+  while (i) {
+    j = Math.floor(Math.random() * i--);
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+
+  return arr;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(nums)
+ * var param_1 = obj.reset()
+ * var param_2 = obj.shuffle()
+ */
+// @lc code=end
